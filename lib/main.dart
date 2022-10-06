7f6ee2503a0c908d851dc0c413b7ad03ae052f8a
Imran <imranlaskarbd@gmail.com>
2022-10-06 19:53:31 +0600

    3rd.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:u1/screen/home_page.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 }
 
 class MyApp extends StatelessWidget {
